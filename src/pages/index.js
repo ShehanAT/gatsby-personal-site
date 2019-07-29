009ad5750627bcda32f05e448d47b593eae240ed
Shehan Atukorala <Shehan@Shehans-MacBook-Pro.local>
2019-07-29 10:12:13 -0400

    added boostrap theme, changed navbar styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import About from "../components/about";
 import Header from "../components/header";
 import Project from "../components/project";

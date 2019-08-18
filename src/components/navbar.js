5139d7fdfe045f955e7f35689e12b1e8f0e8bc83
Shehan Atukorala <Shehan@Shehans-MacBook-Pro.local>
2019-08-17 20:02:25 -0400

    added navbar structure

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,14 +4,20 @@ import React, { Component } from "react"
 export default class Navbar extends Component{
     render(){
       return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav mr-auto">
+        <nav className="navbar ">
+        <div className="navbar-collapse">
+          <ul className="navbar-nav ">
             <li className="nav-item">
               <a className="nav-link" href="#about">About</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#project">Projects</a>
+              <a className="nav-link" href="#work">Work</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#testimonials">Testimonials</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
             </li>
           </ul>
         </div>
